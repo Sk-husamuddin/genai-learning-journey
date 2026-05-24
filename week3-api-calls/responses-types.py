073cb3c0a921554f9ad45db8279f5998f04910fa
Sk Husamuddin <shaikhusamuddin666@gmail.com>
2026-05-24 20:43:22 +0530

    Cheked the complete response from a API key

---
+++ week3-api-calls/responses-types.py
@@ -0,0 +1,21 @@
+import os
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ-API-KEY"))
+
+message=[
+    {
+        "role":"user",
+    "content":"What is API is one line?"
+    }
+]
+
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=message
+)
+
+print(response)
