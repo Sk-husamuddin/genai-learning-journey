a14bdf90e318edcb58ae23fc7d219b87297952b1
Shaik Husamuddin <shaikhusamuddin666@gmail.com>
2026-06-02 21:03:17 +0530

    Learned the concept of chromaDB.

---
+++ week4-chromaDB/chroma-demo.py
@@ -0,0 +1,23 @@
+import chromadb
+
+client = chromadb.Client()
+
+collections = client.get_or_create_collection(name="my_docs")
+
+collections.add(
+    documents=[
+        "Cars are fast vehicles used for transportation",
+        "Dogs are loyal animals and great pets",
+        "Automobiles revolutionized modern travel",
+        "Cats are independent and curious animals",
+        "Electric vehicles are the future of transport",
+    ],
+    ids=["id1","id2","id3","id4","id5"]
+)
+
+results = collections.query(
+    query_texts=["How can i travel faster?"],
+    n_results=3
+)
+
+print(results["documents"][0])
